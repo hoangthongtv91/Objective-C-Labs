b172d22c8fa6b0beb66c3e634dd9ea9d1f3e8278
Thong Hoang Nguyen <thongnguyen@Thongs-MacBook-Pro.local>
2019-03-04 16:04:07 -0800

    Lab8 finished

---
+++ Lab9/Cook.h
@@ -0,0 +1,17 @@
+//
+//  Cook.h
+//  Lab9
+//
+//  Created by Thong Hoang Nguyen on 2019-03-04.
+//  Copyright © 2019 Thong Hoang Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Cook : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lab9/Cook.m
@@ -0,0 +1,13 @@
+//
+//  Cook.m
+//  Lab9
+//
+//  Created by Thong Hoang Nguyen on 2019-03-04.
+//  Copyright © 2019 Thong Hoang Nguyen. All rights reserved.
+//
+
+#import "Cook.h"
+
+@implementation Cook
+
+@end
